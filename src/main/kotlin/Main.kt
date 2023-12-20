4a53f8ae6c29ecfc92f565fafb70d5c5dc580fc4
Kaustubh <kaustubhbhagwat93@gmail.com>
2023-12-20 17:26:57 +0530

    Long

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,15 @@ fun main(args: Array<String>) {
     val maxIntegerValue: Int  = Int.MAX_VALUE
     val minIntegerValue: Int  = Int.MIN_VALUE
     println("Max integer value is $maxIntegerValue \nand Min Integer value is $minIntegerValue")
-    
+
+    val maxShortValue: Short = Short.MAX_VALUE
+    val minShortValue: Short = Short.MIN_VALUE
+    println("Max Short is $maxShortValue \nMin Short value is $minShortValue")
+
+
+    val maxLongValue: Long = Long.MAX_VALUE
+    val minLongValue: Long = Long.MIN_VALUE
+    println("Max Long is $maxLongValue \nMin Long value is $minLongValue")
+
 
 }
