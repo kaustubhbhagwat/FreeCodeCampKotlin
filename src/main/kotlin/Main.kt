3e1f186202b8f5419cfcab1baceda1cd9869c5f4
Kaustubh <kaustubhbhagwat93@gmail.com>
2023-12-20 17:23:26 +0530

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var username = "Kaustubh"
+    var age = 30
+    val maxIntegerValue: Int  = Int.MAX_VALUE
+    val minIntegerValue: Int  = Int.MIN_VALUE
+    println("Max integer value is $maxIntegerValue \nand Min Integer value is $minIntegerValue")
+    
+
+}
