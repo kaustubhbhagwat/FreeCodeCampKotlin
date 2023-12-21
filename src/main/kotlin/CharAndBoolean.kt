d8f1498e99b8b7948c0ae01e30e743dec36776e1
Kaustubh <kaustubhbhagwat93@gmail.com>
2023-12-21 14:18:10 +0530

    Operators

---
+++ src/main/kotlin/CharAndBoolean.kt
@@ -0,0 +1,6 @@
+fun main() {
+
+    val myCharValue: Char = 'D'
+    val myBooleanValue: Boolean = true
+    println("My Char value: myCharValue  and My Boolean value: $myBooleanValue.")
+}
---
+++ src/main/kotlin/Operators.kt
@@ -0,0 +1,30 @@
+fun main() {
+    var x= 5
+    val y = 3.0
+
+
+    //Placeholder expression
+    println("x + y= ${x + y}")
+    println("x - y= ${x - y}")
+    println("x * y= ${x * y}")
+    println("x % y= ${x % y}")
+    println("x / y= ${x / y}")
+
+    var result = x + y
+
+    result =  result+2
+    println(result)
+
+    result =  result-2
+    println(result)
+
+    result =  result*2
+    println(result)
+
+    result =  result/2
+    println(result)
+
+    result =  result%2
+    println(result)
+
+}
